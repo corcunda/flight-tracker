63599e129623277a3c13021253c82d92de1d9e31
corcunda <joao@idwebstudio.com>
2024-12-29 20:18:01 -0300

    feature: added header on front

--- resources/vue/App.vue
+++ resources/vue/App.vue
@@ -1,15 +1,43 @@
 <template>
     <div class="main-wrapper">
-        <RouterView />
+        <Header></Header>
+        <div class="component-wrapper">
+            <RouterView />
+        </div>
     </div>
 </template>
 
 <script>
+import { useUserStore } from '@/stores/user';
+import { useAuthStore } from '@/stores/auth';
+import Header from "@/components/Header.vue";
 import {RouterView} from "vue-router";
 export default {
     name: 'App',
+    setup() {
+        const authStore = useAuthStore();
+        const userStore = useUserStore();
+
+        return { authStore, userStore };
+        
+    },
     components: {
-        RouterView
+        Header,
+        RouterView,
+    },
+    watch: {
+        propSelected(newValue, oldValue) {
+            this.handleSelectionDropdown();
+        },
+    },
+    async mounted() {
+        if (this.authStore.getIsAuthenticated) {
+            // If authenticated, fetch user info
+            await this.userStore.fetchUser();
+        } else {
+            // If not authenticated, redirect to the login page
+            this.$router.push('/');
+        }
     },
 };
 </script>
@@ -17,6 +45,17 @@ export default {
 <style>
 body{
     background: #333;
+}
+header{
+    background: #fff;
+    width: 100%;
+    height: 100px;
+    position: fixed;
+    top: 0;
+    left: 0;
+}
+.component-wrapper{
+    margin-top: 100px;
 }
 </style>
 <style scoped>
---
+++ resources/vue/components/Header.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+        <header>
+            <button v-if="authStore.isAuthenticated" @click="logout">Logout</button>
+        </header>
+    </div>
+</template>
+  
+<script>
+import { useAuthStore } from '@/stores/auth'; // Adjust the path to your store
+export default {
+    name: 'Header',
+    setup() {
+        const authStore = useAuthStore();
+
+        return { authStore };
+    },
+    data() {
+        return {
+            
+        };
+    },
+    methods: {
+        logout() {
+            let vm = this;
+            vm.authStore.logout()
+                .then(response => {
+                    // console.log('OK', response);
+                    vm.$router.push('/');
+                })
+                .catch(error => {
+                    console.log('NOT OK => ', error.response);
+                })
+                .finally(() => {
+                });
+        },
+    },
+
+};
+</script>
